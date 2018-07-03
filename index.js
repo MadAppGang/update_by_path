71bc5dc2b44e4c4a4962c48a3fb768442072fb3e
dprovodnikov <wireden@yandex.ru>
2018-07-03 23:45:49 +0300

    Add an ability to query array elements by prop value

--- index.js
+++ index.js
@@ -1,33 +1,22 @@
-const isObject = o => typeof o === 'object' 
+const isObject = value => typeof value === 'object';
 const isFunction = f => typeof f === 'function';
-const isNumber = n => typeof Number(n) === 'number';
+const isNumber = n => typeof Number(n) === 'number' && !isNaN(n);
+const isUndefined = v => typeof v === 'undefined';
 const firstOf = arr => arr[0];
 const copyOf = o => Object.assign({}, o);
-
-const replaceByIndex = (arr, index, nextValue) => {
-  return arr.map((value, i) => {
-    if (index !== i) {
-      return value;
-    }
-
-    return nextValue;
-  }).filter(value => typeof value !== 'undefined');
-};
-
-const replaceByValue = (arr, currentValue, nextValue) => {
-  return arr.map((value) => {
-    if (value !== currentValue) {
-      return value;
-    }
-
-    return nextValue;
-  }).filter(value => typeof value !== 'undefined');
-};
-
+const reducePath = path => path.split('.').slice(1).join('.');
+const filter = arr => arr.filter(v => !isUndefined(v));
+const replaceByIndex = (arr, index, nextValue) =>
+  filter(arr.map((value, i) => index !== i ? value : nextValue));
+const replaceByValue = (arr, currentValue, nextValue) =>
+  filter(arr.map(value => value !== currentValue ? value : nextValue));
 const getNextValue = (currentValue, _value) =>
   isFunction(_value) ? _value(currentValue) : _value;
 
-const arrayQueryRegex = /\[(.*)\]/;
+const ARRAY_QUERY_REGEX = /\[(\w+)\]/;
+const ARRAY_QUERY_BY_PROP_REGEX = /\[(\w+)=(\w+)\]/;
+
+const purifyNode = node => node.replace(/\[.*\]/, '');
 
 const update = (source, path, value) => {
   if (isObject(path)) {
@@ -39,21 +28,18 @@ const update = (source, path, value) => {
 
   const output = copyOf(source);
   const pathNodes = path.split('.');
-  const reducedPath = pathNodes.slice(1).join('.');
-
   let node = firstOf(pathNodes);
 
   // contains array query?
-  const arrayQueryMatch = node.match(arrayQueryRegex);
-
-  // remove array query
-  node = node.replace(arrayQueryRegex, '');
+  const arrayQueryMatch = node.match(ARRAY_QUERY_REGEX);
 
   const isLastNode = pathNodes.length === 1;
 
   if (isLastNode) {
-    if (arrayQueryMatch && Array.isArray(output[node])) {
-      const arr = output[node];
+    const pureNode = purifyNode(node); // node without array query
+
+    if (arrayQueryMatch && Array.isArray(output[pureNode])) {
+      const arr = output[pureNode];
       const query = arrayQueryMatch[1];
 
       if (isNumber(query)) {
@@ -61,26 +47,59 @@ const update = (source, path, value) => {
         const index = Number(query);
         const currentValue = arr[index];
         const nextValue = getNextValue(currentValue, value);
-        output[node] = replaceByIndex(arr, index, nextValue);
+        output[pureNode] = replaceByIndex(arr, index, nextValue);
       } else {
         // searching by value
         const currentValue = arr.find(v => v === query);
         const nextValue = getNextValue(currentValue, value);
-        output[node] = replaceByValue(arr, query, nextValue);
+        output[pureNode] = replaceByValue(arr, query, nextValue);
       }
 
       return output;
     } else {
-      output[node] = getNextValue(output[node], value);
+      const queryByPropMatch = node.match(ARRAY_QUERY_BY_PROP_REGEX);
+      const arr = output[pureNode];
+
+      if (queryByPropMatch && Array.isArray(output[pureNode])) {
+        const [key, val] = queryByPropMatch.slice(1, 3);
+        const currentValue = arr.find(v => v[key] === val);
+        const nextValue = getNextValue(currentValue, value);
+        output[pureNode] = replaceByValue(arr, currentValue, nextValue);
+      } else {
+        output[pureNode] = getNextValue(output[pureNode], value);
+      }
+
       return output;
     }
   }
 
-  const nextStepValue = isObject(source[node])
-    ? copyOf(output[node])
-    : !isLastNode ? {} : value;
+  const currentValue = output[node];
 
-  output[node] = update(nextStepValue, reducedPath, value);
+  let nextSource;
+
+  if (isObject(currentValue)) {
+    nextSource = copyOf(currentValue);
+  } else {
+    if (isLastNode) {
+      nextSource = value;
+    } else {
+      const queryByPropMatch = node.match(ARRAY_QUERY_BY_PROP_REGEX);
+
+      if (queryByPropMatch) {
+        const pureNode = purifyNode(node); // node without array query
+        const arr = output[pureNode];
+        const [key, val] = queryByPropMatch.slice(1, 3);
+        const currentValue = arr.find(item => item[key] == val);
+        const nextValue = update(currentValue, reducePath(path), value);
+        output[pureNode] = replaceByValue(arr, currentValue, nextValue);
+        return output;
+      } else {
+        nextSource = {};
+      }
+    }
+  }
+
+  output[node] = update(nextSource, reducePath(path), value);
 
   return output;
 };
